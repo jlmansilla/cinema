a727cb4d5db415acb7919939831c9635769ddef7
jlmansilla <juanmancillaster@gmail.com>
2022-11-05 23:58:38 +0000

    add host to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "4e0ea6ca042d4f67ada88e548e36825b.vfs.cloud9.us-east-1.amazonaws.com"
 end
